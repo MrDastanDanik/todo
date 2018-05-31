318c73b2c5b33d6cad67daff48048860e1664ff7
MrDastanDanik <12bignik135@gmail.com>
2018-05-31 18:47:16 +0300

    Small syntax correction

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -10,10 +10,10 @@ use App\Models\Task;
 use Faker\Generator as Faker;
 
 $factory->define(Task::class, function (Faker $faker) {
-    return [
+    return array(
         'user_id' => 1,
-        'text' => $faker->text,
+        'text' => $faker->company,
         'is_done' => $faker->boolean,
-    ];
+    );
 });
 
